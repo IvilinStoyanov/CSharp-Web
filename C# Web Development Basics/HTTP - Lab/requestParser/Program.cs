08ad3149fa56807e7c40297fcbc1cfc0fb028f0f
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-09-23 21:47:48 +0300

    Adding skeleton

---
+++ C# Web Development Basics/HTTP - Lab/requestParser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace requestParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var routes = new Dictionary<string, HashSet<string>>();
+
+            var input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                var splitInput = input.Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+                var httpMethod = splitInput[1];
+                var endPoint = splitInput[0];
+
+                if(!routes.ContainsKey(httpMethod))
+                {
+                    routes.Add(httpMethod, new HashSet<string>());
+                }
+                routes[httpMethod].Add(endPoint);
+
+                input = Console.ReadLine();
+            }
+
+            var requestString = Console.ReadLine();
+        }
+    }
+}
