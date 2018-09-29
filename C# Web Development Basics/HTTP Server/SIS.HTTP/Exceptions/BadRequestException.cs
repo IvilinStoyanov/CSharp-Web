8da93049e89d0e0e7cfd9f82345e43a83ab2e51e
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-09-29 23:45:20 +0300

    till Headers

--- C# Web Development Basics/HTTP Server/SIS.HTTP/Exceptions/BadRequestException.cs
+++ C# Web Development Basics/HTTP Server/SIS.HTTP/Exceptions/BadRequestException.cs
@@ -5,6 +5,14 @@ namespace SIS.HTTP.Exceptions
 {
     public class BadRequestException : Exception
     {
-        public HttpStatusCode StatusCode { get; set; }
+        private const string ErrorMessage = "The Request was malformed or contains unsupported elements";
+
+        public const HttpStatusCode statusCode = HttpStatusCode.BadRequest;
+
+        public BadRequestException()
+            :base(ErrorMessage)
+        {
+
+        }
     }
 }
