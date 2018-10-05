b00208857f9a32e1cfb4b893f9c0fd38524917e1
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-06 00:04:59 +0300

    Adding handmade http server - SIS

---
+++ C# Web Development Basics/WEB SERVER - USING COOKIES/SIS.HTTP/Responses/HttpResponse.cs
@@ -0,0 +1,72 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Cookies;
+using SIS.HTTP.Cookies.Contracts;
+using SIS.HTTP.Extensions;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Headers.Contracts;
+using SIS.HTTP.Responses.Contracts;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SIS.HTTP.Responses
+{
+    public class HttpResponse : IHttpResponse
+    {
+        public HttpResponse() { }
+
+        public HttpResponse(HttpStatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+            this.Cookies = new HttpCookieCollection();
+            this.Headers = new HttpHeaderCollection();
+            this.Content = new byte[0];
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public IHttpHeaderCollection Headers { get; private set; }
+
+        public byte[] Content { get; set; }
+
+        public IHttpCookieCollection Cookies { get; }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            this.Cookies.Add(cookie);
+        }
+
+        public void AddHeader(HttpHeader header)
+        {
+            this.Headers.Add(header);
+        }
+
+        public byte[] GetBytes()
+        {
+            return Encoding
+                .UTF8
+                .GetBytes(this.ToString())
+                .Concat(this.Content)
+                .ToArray();
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result
+                .AppendLine($"{GlobalConstants.HttpOneProtocolFragment} {this.StatusCode.GetResponseLine()}")
+                .AppendLine($"{this.Headers}");
+
+            if(this.Cookies.HasCookie())
+            {
+                result.AppendLine($"{GlobalConstants.CookieResponseHeaderName}: {this.Cookies}");
+            }
+
+            result.AppendLine();
+               
+
+            return result.ToString();
+        }
+    }
+}
---
+++ C# Web Development Basics/WEB SERVER - USING COOKIES/SIS.HTTP/Session/HttpSessions.cs
@@ -0,0 +1,54 @@
+﻿using SIS.HTTP.Session.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace SIS.HTTP.Session
+{
+    public class HttpSessions : IHttpSession
+    {
+        private readonly IDictionary<string, object> parameters;
+
+        public HttpSessions(string id)
+        {
+            this.Id = id;
+            this.parameters = new Dictionary<string, object>();
+        }
+
+        public string Id { get; }
+
+        public object GetParameters(string name)
+        {
+           if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException();
+            }
+
+           if(!this.ContainsParameters(name))
+            {
+                return null;
+            }
+
+            return this.parameters[name];
+        }
+
+        public void AddParameter(string name, object parameter)
+        {
+            if(this.ContainsParameters(name))
+            {
+                throw new ArgumentException();
+            }
+
+            this.parameters[name] = parameters;
+        }
+
+        public void ClearParameters()
+        {
+            this.parameters.Clear();
+        }
+
+        public bool ContainsParameters(string name)
+        {
+            return this.parameters.ContainsKey(name);
+        }
+    }
+}
