443aa6bc7bb4b1ea8fa9298e1387dbb97655c5f9
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-06 23:50:36 +0300

    Adding ef. Creating a database with code-first approach

---
+++ CSharp-Web-Development-Basics/WEB SERVER DATABASES-EF CORE/SIS.HTTP/Exceptions/BadRequestException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        private const string ErrorMessage = "The Request was malformed or contains unsupported elements";
+
+        public const HttpStatusCode statusCode = HttpStatusCode.BadRequest;
+
+        public BadRequestException()
+            :base(ErrorMessage)
+        {
+
+        }
+    }
+}
---
+++ CSharp-Web-Development-Basics/WEB SERVER DATABASES-EF CORE/SIS.HTTP/Responses/Contracts/IHttpResponse.cs
@@ -0,0 +1,25 @@
+﻿using SIS.HTTP.Cookies;
+using SIS.HTTP.Cookies.Contracts;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Headers.Contracts;
+using System.Net;
+
+namespace SIS.HTTP.Responses.Contracts
+{
+   public interface IHttpResponse
+    {
+        HttpStatusCode StatusCode { get; set; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        IHttpCookieCollection Cookies { get; }
+
+        byte[] Content { get; set; }
+
+        void AddHeader(HttpHeader header);
+
+        void AddCookie(HttpCookie cookie);
+
+        byte[] GetBytes();
+    }
+}
