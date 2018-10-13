d89539c39f0559e1ed22ad5e3128a62723ea101b
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-13 18:02:45 +0300

    Add whole project

---
+++ CSharp-Web-Development-Basics/WEB APPLICATION USING HTTP(Handmade)/RunesWebApp/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RunesWebApp.Models
+{
+    public class Album : BaseModel<string>
+    {
+        public Album()
+        {
+            this.Albums = new HashSet<TrackAlbum>();
+        }
+
+        public string Name { get; set; }
+
+        public string Cover { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<TrackAlbum> Albums { get; set; }
+    }
+}
