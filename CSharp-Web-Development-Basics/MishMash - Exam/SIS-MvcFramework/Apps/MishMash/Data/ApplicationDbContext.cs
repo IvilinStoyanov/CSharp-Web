9e53c799926f20e3e8738463c5deb91995c8488c
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-30 12:42:55 +0200

    add database models

---
+++ CSharp-Web-Development-Basics/MishMash - Exam/SIS-MvcFramework/Apps/MishMash/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MishMash.Models;
+
+namespace MishMash.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Channel> Channels { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<UserInChannel> UserInChannel { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=GIGABYTE-PC\SQLEXPRESS;Database=MishMash;Integrated Security=True;");
+        }
+    }
+}
