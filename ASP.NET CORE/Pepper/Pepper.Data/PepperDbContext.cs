577dbcf833d56b22f1e3ae47f91cfa70cd7c449a
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-16 15:11:50 +0200

    add project

---
+++ ASP.NET CORE/Pepper/Pepper.Data/PepperDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pepper.Data.Web.Data
+{
+    public class PepperDbContext : IdentityDbContext
+    {
+        public PepperDbContext(DbContextOptions<PepperDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
