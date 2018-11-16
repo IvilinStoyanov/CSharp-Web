5b9aa96402b017ba7b2b90a8315565f338433450
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-16 18:58:37 +0200

    add product entity and ProductType(enum)

---
+++ ASP.NET CORE/Pepper/Pepper.Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using Pepper.Models.Enums;
+
+namespace Pepper.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductType Type { get; set; }
+    }
+}
