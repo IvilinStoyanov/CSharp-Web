f178fb24bd7ad3553f6edae71914771c58904a68
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-11-27 14:32:06 +0200

    modify user model

---
+++ ASP.NET CORE/Eventures/Eventures.Models/EventuresUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Eventures.Models
+{
+    public class EventuresUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string UniqueCitizenNumber { get; set; }
+    }
+}
